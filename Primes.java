77c85edec0fb5fe3abc6a6e359bdc20a5b5bbcd2
Ofek Katzav <ofekkatzav@h-MacBook-Pro-sl-Ofek.local>
2024-12-01 21:32:56 +0200

    done q2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,29 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+    
+    int n = Integer.parseInt(args[0]);
+    boolean[] isPrime = new boolean[n+1];
+        
+        for (int i = 0; i < isPrime.length; i++) {
+            if(i<=1)
+            isPrime[i] = false;
+            else
+            isPrime[i] = true;
+        }
+        for (int j = 2; j < n; j++) {
+            for(int i = 3; i < isPrime.length; i++) {
+                if(i % j == 0 && i != j ){//if index is even >2
+                    isPrime[i] = false;
+                }
+            }
+        }
+        for (int i = 0; i < isPrime.length; i++) {
+            if(isPrime[i])
+            System.out.println(i);
+        }
     }
+
+
+
 }
+
